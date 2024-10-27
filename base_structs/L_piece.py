37188a96a1faf3afa9cc0fc0f6692a6c249dc9d0
Gandledore <rsmilstrey@gmail.com>
2024-10-27 12:48:34 -0700

    reinterpretation of direction to uniquely define L
    direction provided is short leg direction, not long leg, which allows one to  figure out long leg direction based on the 4x4 limit imposed by the play grid

--- base_structs/L_piece.py
+++ base_structs/L_piece.py
@@ -1,18 +1,37 @@
 import numpy as np
+from typing import Tuple
 
 class L_piece():
-    def compute_L_coords(self):
+    def compute_L_coords(self)->Tuple[np.ndarray[int,int],
+                                      np.ndarray[int,int],
+                                      np.ndarray[int,int],
+                                      np.ndarray[int,int]]:
+        """computes matrix coords of L piece
+            matrix coords:
+                upper left  : [1,1]
+                top right   : [4,1]
+                bottom left : [1,4]
+                bottom right: [4,4]
+            L coords example:
+                
+                | p1 | p2 | p3 |
+                | p0 |
+                
+        Returns:
+            Tuple of 4 ndarrays each containing 2 integers
+        """
+        
         p1 = np.array([self.x,self.y])
-        p2 = p1 + self.direction_mapping[self.d]
-        p3 = p1 + self.direction_mapping[self.d]*2
-        p0 = p1 + self.direction_mapping[self.orientation_map[self.d][self.mirror]]
+        p0 = p1 + self.direction_mapping[self.short_leg_direction]
+        
+        p2 = p1 + self.direction_mapping[self.long_leg_direction]
+        p3 = p1 + self.direction_mapping[self.long_leg_direction]*2
         return p0,p1,p2,p3
     
-    def __init__(self,x:int,y:int,d:str,mirror:bool):
+    def __init__(self,x:int,y:int,d:str):
         self.x = x
         self.y = y
-        self.d = d
-        self.mirror = mirror
+        self.short_leg_direction = d
         
         self.direction_mapping = {
             'N':np.array([0,-1]),
@@ -23,8 +42,13 @@ class L_piece():
         self.orientation_map = {
             'N': ('E', 'W'),
             'E': ('S', 'N'),
-            'S': ('W', 'E'),
-            'W': ('N', 'S')
+            'S': ('E', 'W'),
+            'W': ('S', 'N')
         }
+        
+        if self.short_leg_direction in ['N','S']:
+            self.long_leg_direction = self.orientation_map[self.short_leg_direction][x>2]
+        elif self.short_leg_direction in ['E','W']:
+            self.long_leg_direction = self.orientation_map[self.short_leg_direction][y>2]
         
         self.p0,self.p1,self.p2,self.p3 = self.compute_L_coords()
--- base_structs/gamestate.py
+++ base_structs/gamestate.py
@@ -3,7 +3,7 @@ import token_piece
 
 class gamestate():
     def __init__(self):
-        self.L1 = L_piece(x=1,y=3,d='E',mirror=True)
-        self.L2 = L_piece(x=4,y=2,d='W',mirror=True)
+        self.L1 = L_piece(x=1,y=3,d='E')
+        self.L2 = L_piece(x=4,y=2,d='W')
         self.T1 = token_piece(x=1,y=1)
         self.T2 = token_piece(x=4,y=4)
