610aeec3943469a64d0192b5599467a475c22172
Gandledore <rsmilstrey@gmail.com>
2024-10-26 21:31:45 -0700

    base class structures to work with
    created token piece, which has just x,y coords
    created L_piece which has (x,y,direction,mirror) and also x1,y1,x2,y2,x3,y3,x4,y4 which are the coords it takes up in the grid. these are redundant and represent the same thing.
    created gamestate, which stores 2 L pieces and 2 tokens, and defaults to the same initial state found in the implementation by https://hwwmath.looiwenli.com/l-game

---
+++ base_structs/gamestate.py
@@ -0,0 +1,9 @@
+import L_piece
+import token_piece
+
+class gamestate():
+    def __init__(self):
+        self.L1 = L_piece(x=1,y=3,d='E',mirror=True)
+        self.L2 = L_piece(x=4,y=2,d='W',mirror=True)
+        self.T1 = token_piece(x=1,y=1)
+        self.T2 = token_piece(x=4,y=4)
