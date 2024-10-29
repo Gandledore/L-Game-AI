a447bc590d533ef463c01a480db01f0f9f510347
Jet Lin <jetlin101@gmail.com>
2024-10-28 19:47:56 -0700

    diplay works

--- game.py
+++ game.py
@@ -17,6 +17,20 @@ class Game:
     def display(self):
         board = np.full((4, 4), "XX") # 4 by 4 of empty string
         
+       
+        for px, py in [self.state.L1.p0, self.state.L1.p1, self.state.L1.p2, self.state.L1.p3]:
+            board[3 - (py - 1), px - 1] = "L1" 
+        for px, py in [self.state.L2.p0, self.state.L2.p1, self.state.L2.p2, self.state.L2.p3]:
+            board[3 - (py - 1), px - 1] = "L2" 
+      
+        T1x, T1y = self.state.T1.get_position()
+        board[3 - (T1y - 1), T1x - 1] = "T1"
+
+        T2x, T2y = self.state.T2.get_position()
+        board[3 - (T2y - 1), T2x - 1] = "T2"
+
+        
+
         rows = ["|" + "|".join(f"{cell:>2}" for cell in row) + "|" for row in board]
         #left wall then the row then the ending right wall
         # f"{cell:>2}" align cell to the right > with a width of 2 spaces. 
