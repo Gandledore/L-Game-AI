131d0f857cc28d1028239a4818876e0c3d7188c4
Jet Lin <jetlin101@gmail.com>
2024-11-06 14:51:48 -0800

    Must enter correct values and will continue game

--- classes/game.py
+++ classes/game.py
@@ -55,10 +55,18 @@ class Game:
             wholeMoves = input(f"Player {self.player+1}: Enter xl1 yl1 dl1 tx ty tx ty: ")
         try:
             move_parts = wholeMoves.split()
-            print(len(move_parts))
+     
             if ( (len(move_parts) != 7) and (len(move_parts) != 3) ):
                 raise ValueError("Enter commands in specified format")
             
+            POSSIBLE_SETS = {'0':{1,2,3,4},'1':{1,2,3,4},'2':{'N','E','S','W'}}
+            
+            for i in range(2):
+                if int(move_parts[i]) not in POSSIBLE_SETS[str(i)]:
+                    raise ValueError(f"L piece coordinate out of bounds. {move_parts[i]} is not in {POSSIBLE_SETS[str(i)]}")
+            if move_parts[2] not in POSSIBLE_SETS['2']:
+                raise ValueError(f"L piece direction invalid. {move_parts[2]} is not in {POSSIBLE_SETS['2']}")
+
 
             new_l_pos = (int(move_parts[0]), int(move_parts[1]),move_parts[2])
 
