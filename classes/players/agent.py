3531d6417b3bd89a7538c1cc0a39c39a0c9d0539
Jet Lin <jetlin101@gmail.com>
2024-11-13 15:04:04 -0800

    quick implmentation not tested does not work

--- classes/players/agent.py
+++ classes/players/agent.py
@@ -4,28 +4,53 @@ from classes.game import Game
 
 class Agent(Player):
     def getMove(self, game: Game) -> action:
-        legal_moves = game.getLegalMoves(game.state)
-        return legal_moves[0] if legal_moves else None
+        # legal_moves = game.getLegalMoves(game.state)
+        # return legal_moves[0] if legal_moves else None
+        state = game.state
+        bestAction = MinimaxSearch(game, state)
+        return bestAction
 
     # function MINIMAX-SEARCH(game, state) returns an action
+    def MinimaxSearch(game: Game, state: state) -> action:
     #     player <- game.TO-MOVE(state)
+        player = game.player
     #     value, move <- MAX-VALUE(game, state)
+        value, move = MaxValue(game, state)
     #     return move
+        return move
 
     # function MAX-VALUE(game, state) returns a (utility, move) pair
+    def MaxValue(game, state) -> (value, move):
     #     if game.IS-TERMINAL(state) then return game.UTILITY(state, player), null
+        if game.isGoal(state):
+            return (game.whoWins(state), None) # value is 0 -1 or 1
     #     v <- -inf
+        v = -float('inf')
     #     for each a in game.ACTIONS(state) do
+        for action in game.getLegalMoves(state):
     #         v2, a2 <- MIN-VALUE(game, game.RESULT(state, a))
+            v2, a2 = MinValue(game, game.getSuccessor(state, action))
     #         if v2 > v then
+            if v2 > v:
     #             v, move <- v2, a
+                v, move = v2, a2
     #     return v, move
+        return v, move
 
     # function MIN-VALUE(game, state) returns a (utility, move) pair
+    def MinValue(game, state) -> (value, move):
     #     if game.IS-TERMINAL(state) then return game.UTILITY(state, player), null
+        if game.isGoal(state):
+            return (game.whoWins(state), None)
     #     v <- +inf
+        v = float('inf')
     #     for each a in game.ACTIONS(state) do
+        for action in game.getLegalMoves(state):
     #         v2, a2 <- MAX-VALUE(game, game.RESULT(state, a))
+            v2, a2 = MaxValue(game, game.getSuccessor(state, action))
     #         if v2 < v then
+            if v2 < v:
     #         v, move <- v2, a
+                v, move = v2, a2
     #     return v, move
+        return v, move
