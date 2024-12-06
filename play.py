6384c2d6868015ca1dd1b715639b02518c60a70b
Gandledore <rsmilstrey@gmail.com>
2024-12-05 20:09:23 -0800

    remove unecessary files, set gamemode returns as expected

--- play.py
+++ play.py
@@ -53,6 +53,7 @@ def setGameMode(p1,p2)->Tuple[np.ndarray[bool],np.ndarray[Players.Player]]:
     player1 = player_dict[p1[0]](0) if p1[1]==None else player_dict[p1[0]](0,p1[1],p1[2])
     player2 = player_dict[p2[0]](1) if p2[1]==None else player_dict[p2[0]](1,p2[1],p2[2])
     players = np.array([player1,player2])
+    return np.array([p1[0]==2,p2[0]==2]),players
 
 def play(gm:Tuple[Tuple[int,Optional[int],Optional[int]],Tuple[int,Optional[int],Optional[int]]]=None,N:int=1,display=True)->Tuple[np.ndarray,np.ndarray,List[List[float]]]:
     game = Game()
